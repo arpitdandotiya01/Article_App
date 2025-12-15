abd88fc87e80e74bde5a27fdc9fb5c1e6b6f663b
Arpit <arpitd@kpit.com>
2025-12-15 14:18:23 +0530

    You can add Article using browser

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,17 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :desc))
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
